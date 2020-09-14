d737b4c9234c2cfafc2705e9bf4b9ec98e4332ae
cokoyoh <charlesokoyoh@gmail.com>
2020-09-14 15:22:38 +0300

    First test

---
+++ test/multiply.test.js
@@ -0,0 +1,10 @@
+const multiply = require('../multiply');
+
+describe('test multiply positive scenarios', () => {
+  test('multiply 3 * 2 should equal to 6', () => {
+    expect(multiply(3, 2)).toBe(6);
+    expect(multiply(3, 2)).not.toBe(5);
+  });
+});
+
+

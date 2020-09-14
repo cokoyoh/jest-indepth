fac8747088fcae6fd154effc4a65c31608c685f5
cokoyoh <charlesokoyoh@gmail.com>
2020-09-14 15:40:59 +0300

    Expanded matchers

---
+++ test/matchersExpanded.test.js
@@ -0,0 +1,32 @@
+const multiply = require('../multiply');
+
+test('there is  pool in Liverpool', () => {
+  expect('Liverpool').toMatch(/pool/);
+});
+
+test('multiply 3 * 2 should equal to 6', () => {
+  expect(multiply(3, 2)).toBe(6);
+  expect(multiply(3, 2)).toBeGreaterThan(5);
+  expect(multiply(3, 2)).toBeLessThan(7);
+  expect(multiply(3, 2)).toBeLessThanOrEqual(6);
+});
+
+test('null', () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+}); 
+
+const carStock = [
+  'BMW',
+  'Mercedes',
+  'Ferrari',
+  'Toyota'
+];
+
+test('that the car stock list has a Ferrari', () => {
+  expect(carStock).toContain('Ferrari');
+});
